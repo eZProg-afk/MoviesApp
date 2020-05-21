29979e5daaddf71984c5713d95f47d4be082db85
spiral <t3stform@yandex.ru>
2020-05-21 16:03:19 +0300

    Last Commit

--- app/src/main/java/spiral/movies/Activities/MainActivity.java
+++ app/src/main/java/spiral/movies/Activities/MainActivity.java
@@ -1,15 +1,81 @@
 package spiral.movies.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import spiral.movies.Data.MovieAdapter;
+import spiral.movies.Model.Movie;
 import spiral.movies.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewMovies;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewMovies = findViewById(R.id.recycler_view_movies);
+        recyclerViewMovies.setHasFixedSize(true);
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+        recyclerViewMovies.setLayoutManager(new LinearLayoutManager(this));
+        getMovies();
+    }
+
+    private void getMovies() {
+        String url = "http://www.omdbapi.com/?s=Batman&apikey=5ba50878";
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("Search");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        String title = obj.getString("Title");
+                        String year = obj.getString("Year");
+                        String posterURL = obj.getString("Poster");
+
+                        Movie movie = new Movie();
+                        movie.setTitle(title);
+                        movie.setYear(year);
+                        movie.setPosterURL(posterURL);
+
+                        movies.add(movie);
+                    }
+                    movieAdapter = new MovieAdapter(MainActivity.this, movies);
+                    recyclerViewMovies.setAdapter(movieAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
     }
 }
