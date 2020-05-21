0f26ed2e16aa9501055ea7e7475c096acdba804e
spiral <t3stform@yandex.ru>
2020-05-21 15:24:59 +0300

    Init commit

---
+++ app/src/main/java/spiral/movies/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package spiral.movies.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import spiral.movies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
